fe46dd3fa8d7b1e9f4668fc94ae2852744caf6a3
stevembugua <113033981+stevembugua@users.noreply.github.com>
2023-01-25 09:01:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data,setData] = useState([])
   const [selected, setSelected] = useState([])
   useEffect(()=>{
-    fetch(" https://www.npoint.io/docs/a08b7d7af9675f4acb31")
+    fetch(" https://stevembugua.github.io/stevebots.github.io/db.json")
     .then(res=>res.json())
     .then(response=>{
       setData(response)
@@ -16,7 +16,7 @@ function App() {
   //delete a bot
 
   const deleteFromFile  = (id)=>{
-    fetch("https://www.npoint.io/docs/a08b7d7af9675f4acb31/" + id, {
+    fetch("https://stevembugua.github.io/stevebots.github.io/db.json/" + id, {
       method: "DELETE",
     })
       .then(res => res.json())
